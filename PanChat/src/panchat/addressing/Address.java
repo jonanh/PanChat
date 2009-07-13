5551a38fddbc48cd43ccab37392effe922ec2d7f
Jon Ander Hernández <jonan.h@gmail.com>
2009-07-13 14:52:36 +0000

    panchat/addressing/Address.java: String nickName;

--- PanChat/src/panchat/addressing/Address.java
+++ PanChat/src/panchat/addressing/Address.java
@@ -4,9 +4,10 @@ import java.io.Serializable;
 import java.util.UUID;
 
 public class Address implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	UUID uuid;
 	String ip;
+	transient String nickName;
 }
