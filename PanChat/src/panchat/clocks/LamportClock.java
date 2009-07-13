1a47214b25f0a04c74a424eca58ccb638baaba31
Jon Ander Hernández <jonan.h@gmail.com>
2009-07-13 14:13:31 +0000

    panchat.clocks:*.java: Initial commit

---
+++ PanChat/src/panchat/clocks/LamportClock.java
@@ -0,0 +1,53 @@
+package panchat.clocks;
+
+/**
+ * Implementación de un reloj lógico de Lamport.
+ * 
+ */
+public class LamportClock {
+	private int c;
+
+	/**
+	 * Inicialización.
+	 * 
+	 * El reloj se iniciará a 1.
+	 */
+	public LamportClock() {
+		c = 1;
+	}
+
+	/**
+	 * @return devuelve el valor del reloj lógico.
+	 */
+	public int getValue() {
+		return c;
+	}
+
+	/**
+	 * Incrementamos el valor del reloj lógico
+	 */
+	public void tick() {
+		c += 1;
+	}
+
+	/**
+	 * Evento al enviar
+	 */
+	public void sendAction() {
+		tick();
+	}
+
+	/**
+	 * Evento al recibir
+	 */
+	public void receiveAction(LamportClock receivedClock) {
+		/*
+		 * Actualizamos el reloj lógico asignando el máximo valor entre el valor
+		 * de este reloj y el reloj recibido.
+		 */
+		if (c < receivedClock.c)
+			c = receivedClock.c;
+
+		tick();
+	}
+}
---
+++ PanChat/src/panchat/clocks/VectorClock.java
@@ -0,0 +1,106 @@
+package panchat.clocks;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import panchat.addressing.Address;
+
+/**
+ * Implementación de un vector de relojes lógicos de Lamport.
+ * 
+ * @author Jon Ander Hernández & Javier Mediavilla
+ */
+public class VectorClock implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private HashMap<Address, LamportClock> clock;
+
+	private Address myId;
+
+	/**
+	 * Inicialización y construcción de la clase VectorClock
+	 * 
+	 * @param numProc
+	 *            Número de procesos en el vector
+	 * @param id
+	 *            Identificador de nuestro proceso
+	 */
+	public VectorClock(int numProc, Address id) {
+
+		myId = id;
+
+		// Inicializamos la hashmap
+		clock = new HashMap<Address, LamportClock>();
+
+		// Añadimos un primer LamportClock asociado a nuestro identificador.
+		clock.put(myId, new LamportClock());
+
+		// Lo autoincrementamos para inicializarlo a 1.
+		clock.get(id).tick();
+	}
+
+	/**
+	 * Incrementamos el valor del vector lógico
+	 */
+	public void tick() {
+		clock.get(myId).tick();
+	}
+
+	/**
+	 * Evento al enviar
+	 */
+	public void sendAction() {
+		tick();
+	}
+
+	/**
+	 * Evento al recibir
+	 */
+	public void receiveAction(VectorClock receivedClock) {
+
+		Entry<Address, LamportClock> entry;
+
+		Iterator<Entry<Address, LamportClock>> iter = receivedClock.clock
+				.entrySet().iterator();
+
+		// Para cada elemento del reloj recivido
+		while (iter.hasNext()) {
+			entry = iter.next();
+
+			// Obtenemos el reloj asociado a la dirección procesada actualmente
+			LamportClock lClock = clock.get(entry.getKey());
+
+			// Comprobamos si existe esta direccion en nuestro vector
+			if (lClock != null)
+				/*
+				 * Si existía un reloj para dicha dirección, actualizamos el
+				 * reloj de nuestro vector.
+				 */
+				lClock.receiveAction(entry.getValue());
+			else
+				/*
+				 * Si no existía, añadimos el reloj a nuestro vector.
+				 */
+				clock.put(entry.getKey(), entry.getValue());
+
+		}
+
+		// Autoincrementamos el valor de nuestro reloj.
+		tick();
+	}
+
+	/**
+	 * Devuelve el valor de un elemento del vector lógico.
+	 * 
+	 * @param i
+	 *            Identificador del proceso
+	 * 
+	 * @return
+	 */
+	public int getValue(int i) {
+		return clock.get(i).getValue();
+	}
+}
