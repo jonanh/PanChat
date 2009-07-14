d6c8e44982ffc2894c0010fe431811dcdf021911
Jon Ander Hernández <jonan.h@gmail.com>
2009-07-14 16:39:59 +0000

    Panchat/Panchat.java: Initial Commit

---
+++ PanChat/src/panchat/Panchat.java
@@ -0,0 +1,45 @@
+package panchat;
+
+import panchat.addressing.ListaCanales;
+import panchat.addressing.ListaUsuarios;
+import panchat.addressing.Usuario;
+
+public class Panchat {
+	private ListaUsuarios listaUsuarios;
+	private ListaCanales listaCanales;
+	private Usuario usuario;
+	
+	/**
+	 * 
+	 * @param usuario
+	 */
+	public Panchat(Usuario usuario){
+		this.listaUsuarios = new ListaUsuarios();
+		this.listaCanales = new ListaCanales();	
+		this.usuario = usuario;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public ListaUsuarios getListaUsuarios() {
+		return listaUsuarios;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public ListaCanales getListaCanales() {
+		return listaCanales;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+}
