ed55d3c82569495640059cb44d09107eb3a3750e
Jon Ander Hernández <jonan.h@gmail.com>
2009-07-14 22:39:37 +0000

    Test.java: Actualizado al nuevo API

--- PanChat/src/interfaz/conexiones/Test.java
+++ PanChat/src/interfaz/conexiones/Test.java
@@ -26,16 +26,27 @@ public class Test {
 		listaUsuarios.add(new Usuario("127.0.0.1", 50003, "Imanol"));
 		listaUsuarios.add(new Usuario("127.0.0.1", 50004, "Nagore"));
 
-		// Creamos un listado de canales
-		LinkedList<Canal> listaCanales = new LinkedList<Canal>();
-		listaCanales.add(new Canal("Locos", listaUsuarios));
-		listaCanales.add(new Canal("Intrepidos", listaUsuarios.subList(1, 3)));
-		listaCanales
-				.add(new Canal("Programadores", listaUsuarios.subList(2, 4)));
-
 		// Registramos el listado de usuarios en la clase Singleton Conexiones
 		for (Usuario address : listaUsuarios)
 			usuarios.añadirUsuario(address);
+
+		// Creamos un listado de canales
+		LinkedList<Canal> listaCanales = new LinkedList<Canal>();
+
+		Canal canalLocos = new Canal("Locos", usuarios);
+		Canal canalIntrepidos = new Canal("Intrepidos", usuarios);
+		Canal canalProgramadores = new Canal("Programadores", usuarios);
+		
+		for (Usuario usuario : listaUsuarios)
+			canalLocos.anyadirUsuarioConectado(usuario);
+		for (Usuario usuario : listaUsuarios.subList(1, 3))
+			canalIntrepidos.anyadirUsuarioConectado(usuario);
+		for (Usuario usuario : listaUsuarios.subList(2, 4))
+			canalIntrepidos.anyadirUsuarioConectado(usuario);
+
+		listaCanales.add(canalLocos);
+		listaCanales.add(canalIntrepidos);
+		listaCanales.add(canalProgramadores);
 
 		// Registramos el listado de usuarios en la clase Singleton Canales
 		for (Canal canal : listaCanales)
