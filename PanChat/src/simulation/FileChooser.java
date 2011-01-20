d167071f31f815093dd09bf5aeed01d08121036c
Jon Ander Hernández <jonan.h@gmail.com>
2011-01-20 22:27:55 +0000

    Pequeña modificación en el selector de ficheros.

--- PanChat/src/simulation/FileChooser.java
+++ PanChat/src/simulation/FileChooser.java
@@ -17,32 +17,53 @@ import simulation.model.SimulationArrowModel;
 @SuppressWarnings("serial")
 public class FileChooser extends JFileChooser {
 
-	/**
-	 * Esta clase sirve para filtrar ficheros por la extension ".sim"
-	 */
-	FileFilter filter = new FileFilter() {
+	private final static String DESCRIPTION = "Simulation (*.sim)";
+	private final static String[] EXTENSIONS = new String[] { "sim" };
 
-		@Override
-		public boolean accept(File f) {
-			String s = f.getName();
-			int i = s.lastIndexOf('.');
-			String extension = null;
-			if (i > 0 && i < s.length() - 1) {
-				extension = s.substring(i + 1).toLowerCase();
+	class SimpleFileFilter extends FileFilter {
+
+		String[] extensions;
+
+		String description;
+
+		public SimpleFileFilter(String ext) {
+			this(new String[] { ext }, null);
+		}
+
+		public SimpleFileFilter(String[] exts, String descr) {
+			// Clone and lowercase the extensions
+			extensions = new String[exts.length];
+			for (int i = exts.length - 1; i >= 0; i--) {
+				extensions[i] = exts[i].toLowerCase();
 			}
-			if (extension != null && extension.equals("sim"))
+			// Make sure we have a valid (if simplistic) description
+			description = (descr == null ? exts[0] + " files" : descr);
+		}
+
+		public boolean accept(File f) {
+			// We always allow directories, regardless of their extension
+			if (f.isDirectory()) {
 				return true;
+			}
+
+			// Ok, it's a regular file, so check the extension
+			String name = f.getName().toLowerCase();
+			for (int i = extensions.length - 1; i >= 0; i--) {
+				if (name.endsWith(extensions[i])) {
+					return true;
+				}
+			}
 			return false;
 		}
 
-		@Override
 		public String getDescription() {
-			return "";
+			return description;
 		}
-	};
+	}
 
 	private FileChooser() {
-		this.setFileFilter(filter);
+		this.addChoosableFileFilter(new SimpleFileFilter(EXTENSIONS,
+				DESCRIPTION));
 	}
 
 	/**
@@ -88,7 +109,8 @@ public class FileChooser extends JFileChooser {
 	 * 
 	 * @param simulationData
 	 */
-	public static void saveFile(Component parent, SimulationArrowModel simulationData) {
+	public static void saveFile(Component parent,
+			SimulationArrowModel simulationData) {
 		try {
 			FileChooser chooser = new FileChooser();
 
